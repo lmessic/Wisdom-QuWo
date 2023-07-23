ca82c1d84ec665fd409d2679ef8e0fb151e91f78
lmessic <1296601488@qq.com>
2023-07-23 11:31:54 +0800

    chore: 微前端项目改造；

--- config/routes.ts
+++ config/routes.ts
@@ -8,21 +8,25 @@ export default [
     name: 'dashboard',
     component: '@/layouts/index',
     meta: {
-      title: '工作台'
+      title: '工作台',
     },
     routes: [
-      { exact: true, path: '/dashboard',  component: '@/pages/dashboard/index' }
-    ]
+      { exact: true, path: '/dashboard', component: '@/pages/dashboard/index' },
+    ],
   },
   {
     path: '/project',
     name: 'project',
     component: '@/layouts/index',
     meta: {
-      title: '项目'
+      title: '项目',
     },
     routes: [
-      { exact: true, path: '/project', component: '@/pages/project/index' }
-    ]
+      { exact: true, path: '/project', component: '@/pages/project/index' },
+    ],
   },
-]
+  {
+    path: '/vue',
+    microApp: 'vue',
+  },
+];
